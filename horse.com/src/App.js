5fe892209cf2e9cccc3ae4c7cd9cd56fb903e605
Guru1926 <gurukiran.professional@gmail.com>
2022-11-28 18:00:23 +0530

    5

--- horse.com/src/App.js
+++ horse.com/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <Login />
-      <p>Testing gitignore</p>
+      <p>Testing gitignore </p>
+      <p>Testing gitignore </p>
 
     </div>
   );
