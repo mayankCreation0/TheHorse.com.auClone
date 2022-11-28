f5725db5b4c555d3ba91290bbad4a302b0a86c81
Guru1926 <gurukiran.professional@gmail.com>
2022-11-28 18:06:18 +0530

    6

--- horse.com/src/App.js
+++ horse.com/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Login from './components/guru/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <p>Testing gitignore </p>
-      <p>Testing gitignore </p>
+
 
     </div>
   );
