34f2bcd150734e857d1c245c97abe0fc7c7a2c1c
nishantp15 <prajapati.nishant@outlook.com>
2022-12-01 19:02:40 +0530

    Checkout page rev07 paymentgateway

--- horse.com/src/components/CheckoutAndPayment/PaymentGateway.jsx
+++ horse.com/src/components/CheckoutAndPayment/PaymentGateway.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
 import './PaymentGateway.css'
 function PaymentGateway() {
+    let [minute,setMinute] = useState(4)
+    let [seconds,setSeconds] =useState(59)
+    let [runStatus, setRunStatus] = useState(false)
+    useEffect(()=>{
+        // setRunStatus(true)
+        // if(runStatus===true){
+        //     setMinute((state)=>state-1)
+        //     setSeconds(59)
+        // }
+        setInterval(()=>{
+            setSeconds((seconds)=>seconds-1)
+        },1000)
+    },[])
+    
+    let [OTP, setOTP] = useState("");
+    function GetOTP(e){
+        setOTP(e.target.value);
+    }
+
+    let NavigateToHome = useNavigate();
+    function VerifyOTP(){
+        console.log(OTP);
+        NavigateToHome('/');
+    }
+
   return (
     <div id='PaymentGatewayDiv'>
         <div id='visaImage'>
@@ -10,8 +37,14 @@ function PaymentGateway() {
             <p>OTP sent to your registered mobile number</p>
         </div>
         <div id='OTPInput'>
-            <input type="password" placeholder='Enter OTP' />
-            <button>Submit</button>
+            <input type="password" placeholder='Enter OTP' onChange={GetOTP}/>
+            <button onClick={VerifyOTP}>Submit</button>
+        </div>
+        <div>
+            <p id='resendOTP'>Resend OTP</p>
+        </div>
+        <div>
+            <p>OTP will expire in <span>{`${minute}:${seconds}`}</span> minutes</p>
         </div>
         <div>
             <p>Do NOT close or refresh the page</p>
