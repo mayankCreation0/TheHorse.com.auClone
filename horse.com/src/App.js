af48bf5db9c27fb70c4daa878a5fcce42382e398
Guru1926 <gurukiran.professional@gmail.com>
2022-12-02 13:49:02 +0530

    resolving

---
+++ horse.com/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// import Carousel from './components/mayank\'s folder/homepage/Carousel/Carousel';
+// import Footer from './components/mayankFolder/footer/footer'
+// import Homepage from './components/mayankFolder/homepage/Homepage'
+// import Navbar from './components/mayankFolder/navbar/Navbar'
+// import BackdropExample from './components/mayankFolder/button/button';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            {/* <Navbar/> */}
+            {/* <BackdropExample/> */}
+            {/* <Homepage/>
+      <Footer/> */}
+        </div>
+    );
+}
+
+export default App;
