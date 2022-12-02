0e2d80396236ee4fe0d895dbfa598e4c48382398
Pranav Purohit <adi089076@gmail.com>
2022-12-02 15:49:43 +0530

    updated

--- horse.com/src/components/Pranav/store/Store.js
+++ horse.com/src/components/Pranav/store/Store.js
@@ -2,6 +2,6 @@ import React from 'react'
 import {legacy_createStore} from "redux";
 import { myReducer } from '../reducer/reducer';
 
-export const store=legacy_createStore(myReducer);
+export const Store=legacy_createStore(myReducer);
 
 
