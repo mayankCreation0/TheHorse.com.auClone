62d4914697f0c7baa77851d0febcf2d8d1503bfa
Guru1926 <gurukiran.professional@gmail.com>
2022-12-03 21:28:22 +0530

    conflict error resolved

--- horse.com/src/components/guru/Logout.js
+++ horse.com/src/components/guru/Logout.js
@@ -5,6 +5,7 @@ import { AuthContext } from './API/Context';
 const Logout = () => {
     const { isAuth, setIsAuth } = useContext(AuthContext);
     localStorage.setItem("thehorse-token", "");
+    localStorage.setItem("thehorse-name", "");
     setIsAuth({ ...isAuth, data: {}, loggedin: false })
     return (
         <div>
